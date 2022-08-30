d5d8014130d60730452d95c5e0b61e4aaefe4a52
AbhroChowdhury <109684820+AbhroChowdhury@users.noreply.github.com>
2022-08-30 11:25:39 -0600

    Update employees.py

--- employees.py
+++ employees.py
@@ -1,15 +1,10 @@
-employee_list = ["AbhroChowdhury", "RitvikDas", "BenJoss", "CharlzCheng", "DebarthaBag", "GeorgeSergiannis", "RandyHaring", 
-    "AnupFouzder", "AreejKhaddaj", "ArunSaha", "BreannaStadlwieser", "DanaHorne", "DemmyOgunsesan", "DipakDas", "GregJoss", 
-    "HaleyKostyshen", "JacobCharbonneau", "JordanDavies", "KarstenHelbig", "KenSinaga", "LoganHutchison", "MananGajiwala", 
-    "MatthewBrydges", "MinChulKim", "MonirKhan", "NathanLuong", "PaulBernard", "RajivDas", "RusselVea", "RusselVea-Russ", 
-    "ScottBannatyne", "ShahriarAhmed", "SriMadathil-Sri", "StevenGylander", "StevenTran", "StuartMills", "SunjitBhusari",
-    "TannerMcLean-Poll", "TishaMariatThomas", "TommyShum", "AngieSmith", "ErickLiebl", "GaryChambers", "Jonathan Roedel",
-    "JordanSeigel", "HeilynCastro.", "MarvinPuac", "PaolaMejia", "WilliamChaparro", "AnandPatil", "NathanLuong-Dave-old",
-    "Richard Patterson",
+employee_list = ["Employee_names",
 ]
 
 
 """
 To add names, simply type them in with quotation marks  
 Make sure that the name is typed to replicate exactly how it is on their timesheet
+Note: Real employee names have been removed on Github version
 """
+
