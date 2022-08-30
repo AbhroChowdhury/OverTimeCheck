81a1e491ca743d04df3d1e6b51849c21e41eb37e
AbhroChowdhury <109684820+AbhroChowdhury@users.noreply.github.com>
2022-08-30 11:27:13 -0600

    Add files via upload

---
+++ employees_short.py
@@ -0,0 +1,7 @@
+employee_list = ["AbhroChowdhury", "RitvikDas", "BenJoss",]
+
+
+"""
+To add names, simply type them in with quotation marks  
+Make sure that the name is typed to replicate exactly how it is on their timesheet
+"""
